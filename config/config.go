c569649cbb7cfadbce2d985c6600cfe68dc4a074
Zhang Zhe <zzhe@amazon.com>
2023-06-20 11:37:01 +0000

    Init repo

---
+++ config/config.go
@@ -0,0 +1,9 @@
+package config
+
+const (
+	AppRootPath      = "app/single-sns"
+	PipelineRootPath = "infra/single-pipeline"
+	RepoName         = "zhang1980s/cdk-go-lab"
+	RepoBranch       = "main"
+	ConnectionArn    = "arn:aws:codestar-connections:us-east-1:894855526703:connection/f540f093-f106-483e-863d-2b9432dd21bb"
+)
---
+++ pipeline/pipeline.go
@@ -0,0 +1,34 @@
+package cicdlib
+
+import (
+	"cdk-go-lab/config"
+
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/pipelines"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+func NewPipelineStack(scope constructs.Construct, id string, props *awscdk.StackProps) awscdk.Stack {
+
+	stack := awscdk.NewStack(scope, &id, props)
+
+	pipelines.NewCodePipeline(stack, jsii.String("SinglePipeline"), &pipelines.CodePipelineProps{
+		Synth: pipelines.NewCodeBuildStep(jsii.String("SynthStep"), &pipelines.CodeBuildStepProps{
+			Input: pipelines.CodePipelineSource_Connection(jsii.String(config.RepoName), jsii.String(config.RepoBranch), &pipelines.ConnectionSourceOptions{
+				ConnectionArn:        jsii.String(config.ConnectionArn),
+				CodeBuildCloneOutput: jsii.Bool(true),
+				TriggerOnPush:        jsii.Bool(true)}),
+			Commands: jsii.Strings(
+				"npx npm install -g aws-cdk",
+				"goenv install 1.18.3",
+				"npx cdk synth"),
+		}),
+	})
+
+	///	deploy := NewSinglePipelineStage(stack, "DeployStage", nil)
+
+	///	pipeline.AddStage(deploy, nil)
+
+	return stack
+}
