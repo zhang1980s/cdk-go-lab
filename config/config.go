5e5488313290014b195a617117bc96d0db5cae92
Zhang Zhe <zzhe@amazon.com>
2023-06-20 11:50:52 +0000

    update code

--- config/config.go
+++ config/config.go
@@ -4,4 +4,5 @@ const (
 	RepoName      = "zhang1980s/cdk-go-lab"
 	RepoBranch    = "main"
 	ConnectionArn = "arn:aws:codestar-connections:us-east-1:894855526703:connection/f540f093-f106-483e-863d-2b9432dd21bb"
+	PipelineName  = "StackPipeline"
 )
