3447f23ae90f125edda5b68ac810d7a950b49afa
Zhang Zhe <zzhe@amazon.com>
2023-06-21 06:54:41 +0000

    Change connection name

--- config/config.go
+++ config/config.go
@@ -1,8 +1,8 @@
 package config
 
 const (
-	RepoName          = "zhang1980s/cdk-go-lab"
-	RepoBranch        = "main"
-	MainConnectionArn = "arn:aws:codestar-connections:us-east-1:894855526703:connection/f540f093-f106-483e-863d-2b9432dd21bb"
-	PipelineName      = "StackPipeline"
+	RepoName      = "zhang1980s/cdk-go-lab"
+	RepoBranch    = "main"
+	ConnectionArn = "arn:aws:codestar-connections:us-east-1:894855526703:connection/f540f093-f106-483e-863d-2b9432dd21bb"
+	PipelineName  = "StackPipeline"
 )
