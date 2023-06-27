4315fcea8f309b2f9a6ab44f3dcc7020e5096b30
Zhang Zhe <zzhe@amazon.com>
2023-06-27 00:25:45 +0000

    Adding manual approval stage

--- pipeline_main/stage.go
+++ pipeline_main/stage.go
@@ -4,10 +4,16 @@ import (
 	"cdk-go-lab/app_main"
 
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/pipelines"
 	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
 )
 
 type PipelineMainStageProps struct {
+	awscdk.StageProps
+}
+
+type PipelineManualApprovalStageProps struct {
 	awscdk.StageProps
 }
 
@@ -21,6 +27,21 @@ func NewPipelineMainStage(scope constructs.Construct, id string, props *Pipeline
 	stage := awscdk.NewStage(scope, &id, &sprops)
 
 	app_main.NewAppMainStack(stage, "MyAppMainStack", nil)
+
+	return stage
+}
+
+func NewManualApprovalStage(scope constructs.Construct, id string, props *PipelineMainStageProps) awscdk.Stage {
+
+	var sprops awscdk.StageProps
+
+	if props != nil {
+		sprops = props.StageProps
+	}
+
+	stage := awscdk.NewStage(scope, &id, &sprops)
+
+	pipelines.NewManualApprovalStep(jsii.String("ManualApproval"), nil)
 
 	return stage
 }
