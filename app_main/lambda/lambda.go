f1213776c6fb188be4539d10b580aee4fe9d8a1f
Zhang Zhe <zzhe@amazon.com>
2023-07-03 08:02:35 +0000

    Update lambda

--- app_main/lambda/lambda.go
+++ app_main/lambda/lambda.go
@@ -12,7 +12,7 @@ type MyEvent struct {
 }
 
 func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
+	return fmt.Sprintf("Hello %s! new", name.Name), nil
 }
 
 func main() {
